70d05150fab584e2771aa4ea3affd47a4ff25b22
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2020-11-03 23:50:35 +0300

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Mehmet Salih Aslan on 3.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
