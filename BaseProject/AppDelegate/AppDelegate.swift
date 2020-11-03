3e42636ab612ee653d378f85bbe711454f4c60e8
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2020-11-04 00:07:06 +0300

    set starting ViewController programmatically add configurations

--- BaseProject/AppDelegate/AppDelegate.swift
+++ BaseProject/AppDelegate/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let bounds = UIScreen.main.bounds
+        self.window = UIWindow(frame: bounds)
+        let viewController = ViewController()
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
